3370ed020ab5cc3561594bab19754140770edbf2
Lycia Kremer <lyciakremer@gmail.com>
2022-08-18 01:24:39 -0300

    added carousel

--- src/components/AgentSection/index.tsx
+++ src/components/AgentSection/index.tsx
@@ -6,23 +6,15 @@ import Link from 'next/link'
 import Agents from '../Agents'
 import * as S from './style'
 
-const FirstSection = () => {
+const AgentSection = () => {
   return (
     <>
       <S.Section>
         <Container>
           <S.Content>
             <S.Infos>
-              <S.Title>Valorant</S.Title>
-              <S.Details>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
-                molestias perspiciatis a corporis? Similique, sunt. Natus sint
-                quibusdam est quam harum velit aspernatur iusto! Corrupti fugiat
-                at facilis exercitationem ea.
-              </S.Details>
-              <Link href="/">Link</Link>
+              <S.Title>Agents</S.Title>
             </S.Infos>
-            <Image src="/trio-agentes.png" alt="img" width={789} height={704} />
           </S.Content>
           <Agents />
         </Container>
@@ -31,4 +23,4 @@ const FirstSection = () => {
   )
 }
 
-export default FirstSection
+export default AgentSection
